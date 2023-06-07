56ac00655bc15dda2c3568c9326467131fb52b97
TZGyn <ca538901d@mozmail.com>
2023-06-08 03:03:43 +0800

    Setting up packages and config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+	modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+	tailwindcss: {
+		configPath: './tailwind.config.ts',
+	},
+	typescript: {
+		strict: true,
+	},
+	runtimeConfig: {
+		public: {
+			api: 'http://dev.local:8000/api',
+			url: 'http://dev.local:8000',
+		},
+	},
+	ssr: false,
 })
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [],
+	important: false,
+	theme: {
+		extend: {
+			colors: {
+				primary: '#0c0c0d',
+				secondary: '#18181b',
+				darkgray: '#121213',
+				lightgray: '#27272a',
+				white: '#e5e7eb',
+			},
+		},
+	},
+}
