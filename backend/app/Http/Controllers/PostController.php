05159a00612f59c436d2e7cd1e13568aa848ad9a
TZGyn <ca538901d@mozmail.com>
2023-06-20 14:12:56 +0800

    Update backend PostController (request validate & store method)

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -10,51 +10,72 @@ class PostController extends Controller
 {
     public function index(Request $request): Response
     {
-        $lastPost = 0;
+        $request->validate(
+            [
+                'lastPost' => 'required|integer',
+            ]
+        );
 
-        if ($request->lastPost) {
-            $lastPost = $request->lastPost;
-        }
+        $lastPost = $request->lastPost;
 
         $posts = Post::query()
-            // ->select([
-            //     'title',
-            //     'description',
-        // ])
             ->where(column: 'sequence', operator: '>', value: $lastPost)
             ->take(10)
             ->get();
 
-        return response([
-            'status' => 200,
-            'data' => $posts,
-        ]);
+        return response(
+            [
+                'status' => 200,
+                'data' => $posts,
+            ]
+        );
     }
 
     public function store(Request $request): Response
     {
-        $postId = Post::query()->max(column: 'sequence');
+        $request->validate(
+            [
+                'title' => 'required|string',
+                'description' => 'required|string',
+            ]
+        );
 
-        $postId = ! $postId ? 1 : $postId + 1;
+        $user = $request->user();
 
-        Post::query()->create([
-            'title' => "This is Post {$postId}",
-            'description' => "post {$postId} description",
-        ]);
+        if (! ($user instanceof User)) {
+            return response(
+                [
+                    'status' => 400,
+                    'message' => 'Invalid user',
+                ]
+            );
+        }
 
-        return response([
-            'status' => 200,
-            'message' => "Create Post {$postId} Success!",
-        ]);
+        $user->posts()->create(
+            [
+                'title' => $request->title,
+                'description' => $request->description,
+            ]
+        );
+
+        return response(
+            [
+                'status' => 200,
+                'message' => 'Create Post Success!',
+            ]
+        );
     }
 
     public function show(string $id): Response
     {
         $post = Post::query()->find($id);
 
-        return response([
-            'status' => 200,
-            'data' => $post,
-        ]);
+        return response(
+            [
+                'status' => 200,
+                'data' => $post,
+            ]
+        );
+    }
     }
 }
