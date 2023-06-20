67d39b04786c60ba6a3371cfb41dd5ffefa15cda
TZGyn <ca538901d@mozmail.com>
2023-06-20 14:14:11 +0800

    Added frontend @vueuse/nuxt package

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,17 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
-	tailwindcss: {
-		configPath: './tailwind.config.ts',
-	},
-	typescript: {
-		strict: true,
-	},
-	runtimeConfig: {
-		public: {
-			api: 'http://dev.local:8000/api',
-			url: 'http://dev.local:8000',
-		},
-	},
-	ssr: false,
+    modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@vueuse/nuxt'],
+    tailwindcss: {
+        configPath: './tailwind.config.ts',
+    },
+    typescript: {
+        strict: true,
+    },
+    runtimeConfig: {
+        public: {
+            api: 'http://dev.local:8000/api',
+            url: 'http://dev.local:8000',
+        },
+    },
+    ssr: false,
 })
