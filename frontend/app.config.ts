81f8336543ffbab82fbb194ff22ef4953e535867
TZGyn <ca538901d@mozmail.com>
2023-07-07 08:32:17 +0800

    Frontend: add nuxticon alias for home and search

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -1,14 +1,16 @@
 export default defineAppConfig({
-    nuxtIcon: {
-        size: '24px', // default <Icon> size applied
-        class: 'icon', // default <Icon> class applied
-        aliases: {
-            nuxt: 'logos:nuxt-icon',
-            loading: 'svg-spinners:90-ring-with-bg',
-            twitter: 'logos:twitter',
-            github: 'mdi:github',
-            plus: 'mdi:plus',
-            user: 'mdi:account-circle',
-        },
-    },
+	nuxtIcon: {
+		size: '24px', // default <Icon> size applied
+		class: 'icon', // default <Icon> class applied
+		aliases: {
+			nuxt: 'logos:nuxt-icon',
+			loading: 'svg-spinners:90-ring-with-bg',
+			twitter: 'mdi:twitter',
+			github: 'mdi:github',
+			plus: 'mdi:plus',
+			user: 'mdi:account-circle',
+			home: 'material-symbols:home-rounded',
+			search: 'material-symbols:search',
+		},
+	},
 })
