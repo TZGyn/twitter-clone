8f4631e5a2355062c348bf291c090d2652d33bb0
TZGyn <ca538901d@mozmail.com>
2023-07-10 04:13:57 +0800

    Backend: remove title from post

--- backend/database/factories/PostFactory.php
+++ backend/database/factories/PostFactory.php
@@ -17,7 +17,6 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->sentence,
             'description' => collect(fake()->paragraphs)->implode(' '),
             'user_id' => 1,
         ];
--- backend/database/migrations/2023_06_05_145153_create_posts_table.php
+++ backend/database/migrations/2023_06_05_145153_create_posts_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id('sequence');
             $table->uuid('id');
-            $table->string('title');
             $table->foreignUuid('user_id')->constrained(table: 'users', column: 'id');
             $table->longText('description')->nullable(true);
             $table->timestamps();
