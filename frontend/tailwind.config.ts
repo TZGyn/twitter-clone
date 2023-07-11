a98c4cc4bde50e4ac4738a9842e53f9385115866
TZGyn <ca538901d@mozmail.com>
2023-07-12 05:55:59 +0800

    Frontend: format tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [],
-    important: false,
-    theme: {
-        extend: {
-            colors: {
-                primary: '#000000',
-                secondary: '#0c0c0d',
-                darkgray: '#18181b',
-                lightgray: '#27272a',
-                light: '#e5e7eb',
-            },
-        },
-    },
+	content: [],
+	important: false,
+	theme: {
+		extend: {
+			colors: {
+				primary: '#000000',
+				secondary: '#0c0c0d',
+				darkgray: '#18181b',
+				lightgray: '#27272a',
+				light: '#e5e7eb',
+				accent: '#0ea5e9',
+			},
+		},
+	},
 }
